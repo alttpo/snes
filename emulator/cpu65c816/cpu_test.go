f906a608e7a12318cd3711b756ad27752a112f0f
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2022-04-02 21:31:03 -0500

    cpu: better screen output from fake PPU

--- emulator/cpu65c816/cpu_test.go
+++ emulator/cpu65c816/cpu_test.go
@@ -10,29 +10,29 @@ import (
 
 func TestCPU_Step(t *testing.T) {
 	roms := []string{
-		"SNES/CPUTest/CPU/BRA/CPUBRA.sfc",
+		//"SNES/CPUTest/CPU/BRA/CPUBRA.sfc",
 		"SNES/CPUTest/CPU/ROR/CPUROR.sfc",
-		"SNES/CPUTest/CPU/CMP/CPUCMP.sfc",
-		"SNES/CPUTest/CPU/RET/CPURET.sfc",
-		"SNES/CPUTest/CPU/INC/CPUINC.sfc",
-		"SNES/CPUTest/CPU/TRN/CPUTRN.sfc",
-		"SNES/CPUTest/CPU/SBC/CPUSBC.sfc",
-		"SNES/CPUTest/CPU/BIT/CPUBIT.sfc",
-		"SNES/CPUTest/CPU/ASL/CPUASL.sfc",
-		"SNES/CPUTest/CPU/LDR/CPULDR.sfc",
-		"SNES/CPUTest/CPU/ORA/CPUORA.sfc",
-		"SNES/CPUTest/CPU/JMP/CPUJMP.sfc",
-		"SNES/CPUTest/CPU/PHL/CPUPHL.sfc",
-		"SNES/CPUTest/CPU/AND/CPUAND.sfc",
-		"SNES/CPUTest/CPU/ROL/CPUROL.sfc",
-		"SNES/CPUTest/CPU/ADC/CPUADC.sfc",
-		"SNES/CPUTest/CPU/MSC/CPUMSC.sfc",
-		"SNES/CPUTest/CPU/DEC/CPUDEC.sfc",
-		"SNES/CPUTest/CPU/PSR/CPUPSR.sfc",
-		"SNES/CPUTest/CPU/STR/CPUSTR.sfc",
-		"SNES/CPUTest/CPU/LSR/CPULSR.sfc",
-		"SNES/CPUTest/CPU/EOR/CPUEOR.sfc",
-		"SNES/CPUTest/CPU/MOV/CPUMOV.sfc",
+		//"SNES/CPUTest/CPU/CMP/CPUCMP.sfc",
+		//"SNES/CPUTest/CPU/RET/CPURET.sfc",
+		//"SNES/CPUTest/CPU/INC/CPUINC.sfc",
+		//"SNES/CPUTest/CPU/TRN/CPUTRN.sfc",
+		//"SNES/CPUTest/CPU/SBC/CPUSBC.sfc",
+		//"SNES/CPUTest/CPU/BIT/CPUBIT.sfc",
+		//"SNES/CPUTest/CPU/ASL/CPUASL.sfc",
+		//"SNES/CPUTest/CPU/LDR/CPULDR.sfc",
+		//"SNES/CPUTest/CPU/ORA/CPUORA.sfc",
+		//"SNES/CPUTest/CPU/JMP/CPUJMP.sfc",
+		//"SNES/CPUTest/CPU/PHL/CPUPHL.sfc",
+		//"SNES/CPUTest/CPU/AND/CPUAND.sfc",
+		//"SNES/CPUTest/CPU/ROL/CPUROL.sfc",
+		//"SNES/CPUTest/CPU/ADC/CPUADC.sfc",
+		//"SNES/CPUTest/CPU/MSC/CPUMSC.sfc",
+		//"SNES/CPUTest/CPU/DEC/CPUDEC.sfc",
+		//"SNES/CPUTest/CPU/PSR/CPUPSR.sfc",
+		//"SNES/CPUTest/CPU/STR/CPUSTR.sfc",
+		//"SNES/CPUTest/CPU/LSR/CPULSR.sfc",
+		//"SNES/CPUTest/CPU/EOR/CPUEOR.sfc",
+		//"SNES/CPUTest/CPU/MOV/CPUMOV.sfc",
 	}
 	for _, romName := range roms {
 		t.Run(romName, func(t *testing.T) {
@@ -75,9 +75,10 @@ func TestCPU_Step(t *testing.T) {
 			}
 
 			s.CPU.Reset()
-			for s.CPU.AllCycles < 0x10_0000 {
+			for s.CPU.AllCycles < 0x1000_0000 {
 				s.CPU.Step()
 			}
+			mmio.printScreen()
 		})
 	}
 }
@@ -195,12 +196,15 @@ func createSNES() (s *snes, err error) {
 }
 
 type testMMIO struct {
-	nmi byte
+	nmi  byte
+	scr  [0x10000]byte
+	addr uint16
 }
 
 func (m *testMMIO) Read(address uint32) (value byte) {
 	//defer func() { fmt.Printf("[$%06x] -> $%02x\n", address, value) }()
 	if address&0xFFFF == 0x4210 {
+		m.printScreen()
 		if m.nmi == 0x42 {
 			m.nmi = 0xc2
 			value = m.nmi
@@ -223,15 +227,24 @@ func (m *testMMIO) Write(address uint32, value byte) {
 	offs := address & 0xFFFF
 	//fmt.Printf("[$%04x] <- $%02x\n", offs, value)
 
-	if offs == 0x4210 {
-		m.nmi = value
-		return
+	//if offs == 0x4210 {
+	//	m.nmi = value
+	//	return
+	//}
+	if offs == 0x2116 {
+		// VMADDL
+		m.addr = uint16(value) | m.addr&0xFF00
+		//fmt.Fprintf(os.Stdout, "$%04x\n", m.addr)
 	}
 	if offs == 0x2117 {
-		fmt.Fprint(os.Stdout, "\n")
+		// VMADDH
+		m.addr = uint16(value)<<8 | m.addr&0x00FF
+		//fmt.Fprintf(os.Stdout, "$%04x\n", m.addr)
 	}
 	if offs == 0x2118 {
-		fmt.Fprintf(os.Stdout, "%c", value)
+		//fmt.Fprintf(os.Stdout, "%c", value)
+		m.scr[m.addr] = value
+		m.addr++
 	}
 }
 
@@ -253,4 +266,14 @@ func (m *testMMIO) Clear() {
 func (m *testMMIO) Dump(address uint32) []byte {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (m *testMMIO) printScreen() {
+	fmt.Println("--------------------------------")
+	for y := 0; y < 32; y++ {
+		for x := 0; x < 32; x++ {
+			fmt.Printf("%c", m.scr[0x7C00+y<<5+x])
+		}
+		fmt.Println()
+	}
 }
