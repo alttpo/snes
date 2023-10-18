dc5e749673f54e4ee30a815382385f238ede0e99
jsd1982 <james.jdunne@gmail.com>
2023-10-18 13:46:13 -0500

    emulator: lowering *RAM to RAM to avoid unnecessary allocation

--- emulator/memory/ram.go
+++ emulator/memory/ram.go
@@ -5,32 +5,32 @@ type RAM struct {
 	offset uint32
 }
 
-func NewRAM(data []byte, offset uint32) *RAM {
-	return &RAM{data, offset}
+func NewRAM(data []byte, offset uint32) RAM {
+	return RAM{data, offset}
 }
 
-func (m *RAM) Read(address uint32) byte {
+func (m RAM) Read(address uint32) byte {
 	return m.data[address-m.offset]
 }
 
-func (m *RAM) Write(address uint32, value byte) {
+func (m RAM) Write(address uint32, value byte) {
 	m.data[address-m.offset] = value
 }
 
-func (m *RAM) Shutdown() {
+func (m RAM) Shutdown() {
 	panic("implement me")
 }
 
-func (m *RAM) Size() uint32 {
+func (m RAM) Size() uint32 {
 	return uint32(len(m.data))
 }
 
-func (m *RAM) Clear() {
+func (m RAM) Clear() {
 	for i := range m.data {
 		m.data[i] = 0
 	}
 }
 
-func (m *RAM) Dump(address uint32) []byte {
+func (m RAM) Dump(address uint32) []byte {
 	panic("implement me")
 }
